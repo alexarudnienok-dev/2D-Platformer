a65dca613c2a3313b7c9748204d9b79e5f7deeba
alexarudnienok-dev <alexarudnienok@gmail.com>
2026-03-26 09:43:17 +0100

    new changes

--- Assets/PlayerMotor.cs
+++ Assets/PlayerMotor.cs
@@ -7,17 +7,49 @@ public class PlayerMotor : MonoBehaviour
     Vector2 direction;
     private bool canJump = true;
     private Rigidbody2D rigidbody2D;
-    public float speed = 10;
-    public float jumpForce = 10;
+    public float speed = 5;
+    public float jumpForce = 5;
+    public float maxSpeed = 10;
+    public float stoppingForce = 10;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
     }
     // Update is called once per frame
-    private void Update()
+    private void FixedUpdate()
+
     {
-        transform.position += new Vector3(direction.x, direction.y, 0) * Time.deltaTime * speed;
+        Playermovement();
+        HandleMaxSpeed();
+        PlayerStopping();
+    }
+
+    private void PlayerStopping()
+    {
+        if (direction.x == 0 && rigidbody2D.linearVelocityX != 0)
+        {
+            rigidbody2D.AddForce(new Vector2(-rigidbody2D.linearVelocityX * stoppingForce, 0));
+
+        }
+    }
+
+    private void HandleMaxSpeed()
+    {
+        if (rigidbody2D.linearVelocityX >= maxSpeed)
+        {
+            rigidbody2D.linearVelocityX = maxSpeed;
+        }
+
+        else if (rigidbody2D.linearVelocityX <= -maxSpeed)
+        {
+            rigidbody2D.linearVelocityX = -maxSpeed;
+        }
+    }
+
+    private void Playermovement()
+    {
+        rigidbody2D.AddForce(new Vector2(direction.x, 0) * speed);
     }
 
     private void OnMove(InputValue value)
